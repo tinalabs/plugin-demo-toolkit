a8bc20323615d944984ce22e944d992f5d4f8d14
Logan Anderson <landerson2@upei.ca>
2020-11-23 12:00:02 -0400

    added basic toc styles

--- packages/tinacms-docs-toolkit/src/components/layout.tsx
+++ packages/tinacms-docs-toolkit/src/components/layout.tsx
@@ -8,14 +8,8 @@ import Loader from "./Loader";
 import ErrorRenderer from "./ErrorRenderer";
 import DocsRichText from "./RichText";
 import styled, { css } from "styled-components";
-const GlobalText = css`
-  --color-primary: #ec4815;
-`;
-const TextWrapper = styled.div`
-  ${GlobalText}
-  ${DocsRichText}
-  min-height: 68vh;
-`;
+import { DocsTextWrapper } from "./tinaioStyles/DocsTextWrapper";
+import Toc from "./tinaioStyles/toc";
 
 export interface Config {
   pages: Page[];
@@ -72,6 +66,7 @@ export const Layout: React.FC<LayoutProps> = ({ config, currentSlug }) => {
   const [Page, loading, error] = useLoadPage(
     currentPage.loadPage as Promise<DemoPage>
   );
+
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const pageTinaConfig: TinaCMSConfig = Page?.tinaConfig || {};
   const cms = new TinaCMS({
@@ -114,9 +109,28 @@ export const Layout: React.FC<LayoutProps> = ({ config, currentSlug }) => {
                   gridRow: "1",
                 }}
               >
-                <TextWrapper>
-                  <Page.default />
-                </TextWrapper>
+                {/* <DocsLayout>
+                  <TextWrapper>
+                    <Page.default />
+                  </TextWrapper>
+                </DocsLayout> */}
+                <DocsLayout>
+                  <DocsGrid>
+                    <DocGridToc>
+                      <Toc tocItems={["first-steps", "getting-help"]} />
+                    </DocGridToc>
+                    <DocGridContent>
+                      <Page.default />
+                      {/* {(props.prevPage?.slug !== null ||
+                        props.nextPage?.slug !== null) && (
+                        <DocsPagination
+                          prevPage={props.prevPage}
+                          nextPage={props.nextPage}
+                        />
+                      )} */}
+                    </DocGridContent>
+                  </DocsGrid>
+                </DocsLayout>
                 <div style={{ marginRight: "0px", marginTop: "40px" }}>
                   <div
                     style={{
@@ -211,6 +225,121 @@ export const DocGridToc = styled.div`
 
   @media (min-width: 830px) {
     padding-top: 4.5rem;
+  }
+`;
+interface DocsLayoutProps {
+  children: any;
+}
+const DocsLayout = ({ children }: DocsLayoutProps) => {
+  return (
+    <>
+      <DocsLayoutDiv>
+        <DocsTextWrapper>{children}</DocsTextWrapper>
+      </DocsLayoutDiv>
+    </>
+  );
+};
+const Overlay = styled.div<Overlay>`
+  pointer-events: none;
+  display: block;
+  position: fixed;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: var(--color-secondary);
+  transition: all 180ms ease-out;
+  opacity: 0;
+  z-index: 1200;
+
+  ${(props) =>
+    props.open &&
+    css`
+      opacity: 0.7;
+      pointer-events: all;
+    `};
+`;
+const DocsLayoutDiv = styled.div`
+  @media (min-width: 1200px) {
+    position: relative;
+    padding: 0 0 0 16rem;
+
+    ${Overlay} {
+      display: none;
+    }
+  }
+`;
+
+interface Overlay {
+  open: boolean;
+}
+
+export const DocsGrid = styled.div`
+  display: grid;
+  width: 100%;
+  position: relative;
+  grid-auto-columns: minmax(1.5rem, 4rem) minmax(280px, 768px)
+    minmax(1.5rem, 4rem);
+  grid-template-areas:
+    ". header ."
+    ". toc ."
+    ". content .";
+  padding-top: 2rem;
+  padding-bottom: 3rem;
+
+  @media (min-width: 830px) {
+    grid-template-areas:
+      ". header header ."
+      ". content toc .";
+    margin: 0 auto;
+    grid-auto-columns: minmax(2rem, auto) fit-content(768px) 240px
+      minmax(0, auto);
+    grid-column-gap: 2rem;
+  }
+
+  @media (min-width: 1600px) {
+    grid-auto-columns: auto 768px 330px auto;
+    grid-column-gap: 3rem;
+  }
+`;
+
+export const DocGridHeader = styled.div`
+  grid-area: header;
+  width: 100%;
+
+  @media (min-width: 830px) {
+    max-width: none;
+  }
+`;
+
+export const DocGridContent = styled.div`
+  grid-area: content;
+  width: 100%;
+`;
+
+export const DocsPageTitle = styled.h1`
+  font-size: 2rem;
+  line-height: 1.3;
+  letter-spacing: 0.1px;
+  color: var(--color-primary);
+  position: relative;
+  font-family: var(--font-tuner);
+  font-style: normal;
+
+  @media (max-width: 1199px) {
+    margin: 0 0 1.25rem 0 !important;
+  }
+`;
+
+export const DocsNavToggle = styled.div`
+  position: fixed;
+  margin-top: 1.25rem;
+  left: 1rem;
+  z-index: 500;
+
+  @media (min-width: 999px) {
+    display: none;
   }
 `;
 
