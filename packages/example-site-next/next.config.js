8a5d7e220d1e3ebc3c0e31a71d7939245f1acf21
Chris D. Macrae <hello@chrisdmacrae.com>
2020-12-01 18:54:32 -0400

    Update next.config.js

--- packages/example-site-next/next.config.js
+++ packages/example-site-next/next.config.js
@@ -1,3 +1,6 @@
 const { configureNext } = require("tinacms-doc-toolkit");
 
-module.exports = configureNext({basePath: '/tinacms-doc-toolkit',})
+module.exports = configureNext({
+  basePath: '/tinacms-doc-toolkit',
+  assetPrefix: '/tinacms-doc-toolkit/'
+})
