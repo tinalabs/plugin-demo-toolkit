f5c1a0a0afbceb1c3eb4e0adb35b366f927594c1
Chris D. MacRae <hello@chrisdmacrae.com>
2021-01-20 13:29:32 -0400

    Limit automatic resolution to root files

--- packages/example-site-next/next.config.js
+++ packages/example-site-next/next.config.js
@@ -5,7 +5,7 @@ const glob = require('glob');
 const CONTENT_DIR = resolve(process.cwd(), "content");
 const BASE_PATH = process.env.NEXT_BASE_PATH || ""
 const ASSET_PREFIX = process.env.NEXT_ASSET_PREFIX || BASE_PATH
-const PAGES = glob.sync(`${CONTENT_DIR}/**/*.mdx`)
+const PAGES = glob.sync(`${CONTENT_DIR}/*.mdx`)
   .map(pagePath => pagePath
     .replace(CONTENT_DIR, '') // Make relative
     .replace(/\.mdx$/, '') // Remove extension
