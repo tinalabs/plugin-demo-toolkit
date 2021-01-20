ad06dc0e51547ab2d21b6efde83941ece3019271
Chris D. MacRae <hello@chrisdmacrae.com>
2021-01-20 13:09:29 -0400

    Dynamic page resolution

--- packages/example-site-next/next.config.js
+++ packages/example-site-next/next.config.js
@@ -1,9 +1,21 @@
 const { configureNext } = require("tinacms-doc-toolkit");
+const { resolve } = require('path');
+const glob = require('glob');
 
-const BASE_PATH = process.env.NEXT_BASE_PATH || "/"
+const CONTENT_DIR = resolve(process.cwd(), "content");
+const BASE_PATH = process.env.NEXT_BASE_PATH || ""
 const ASSET_PREFIX = process.env.NEXT_ASSET_PREFIX || BASE_PATH
+const PAGES = glob.sync(`${CONTENT_DIR}/**/*.mdx`)
+  .map(pagePath => pagePath
+    .replace(CONTENT_DIR, '') // Make relative
+    .replace(/\.mdx$/, '') // Remove extension
+    .replace(/index$/, '') // Remove trailing index
+  );
 
 module.exports = configureNext({
+  env: {
+    pagePaths: PAGES
+  },
   basePath: BASE_PATH,
   assetPrefix: ASSET_PREFIX
 })
--- packages/example-site-next/tina-demo.config.tsx
+++ packages/example-site-next/tina-demo.config.tsx
@@ -1,26 +1,21 @@
 import Link from "next/link";
 import { Config } from "tinacms-doc-toolkit";
-import { join } from "path";
-import glob from "glob";
 
-const CONTENT_DIR = join(process.cwd(), "./content");
-const PAGES = glob.sync(`${CONTENT_DIR}/**/*.mdx`)
-  .map(pagePath => {
-    const relativePath = pagePath.replace(CONTENT_DIR, '');
-    const slug = relativePath.startsWith('index.mdx')
-      ? "Introduction" // Use introduction for root page
-      : pagePath.replace(CONTENT_DIR, '').replace("index.mdx", '').replace('.mdx', ''); // Use page path as slug
-    const label = slug.replace(/\/([a-Z]{1})/, (value) => value.toUpperCase()); // Capital case slug
+const PAGE_PATHS = Array.isArray(process.env.pagePaths) ? process.env.pagePaths : [];
 
+export const DemoConfig: Config = {
+  pages: PAGE_PATHS.map(slug => {
+    const label = slug === '/'
+      ? "Introduction"
+      : slug.replace("/", " ");
+    const loadPage = import("./content" + slug.replace(/\/$/, '/index') + ".mdx");
+  
     return {
       label,
       slug,
-      loadPage: import(pagePath)
+      loadPage
     }
-  });
-
-export const DemoConfig: Config = {
-  pages: PAGES,
+  }),
   tinaConfig: {
     enabled: true,
   },
