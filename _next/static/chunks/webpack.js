2ab3b4c31ab2e0b72c63a7caab9cf2dd2b89d07a
Chris D. MacRae <hello@chrisdmacrae.com>
2021-01-20 22:53:25 -0400

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d8b5cc82621f225f7147";
+/******/ 	var hotCurrentHash = "2dd506f77a8a71cd5261";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
