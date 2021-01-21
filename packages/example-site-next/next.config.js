042357515741115094e723526c4afc7fb2ab45ea
Chris D. MacRae <hello@chrisdmacrae.com>
2021-01-20 23:10:44 -0400

    Fix gh-pages deploy logic and naming of packages

--- packages/example-site-next/next.config.js
+++ packages/example-site-next/next.config.js
@@ -2,10 +2,10 @@ const { configureNext } = require("tinacms-doc-toolkit");
 const { resolve } = require('path');
 const glob = require('glob');
 
-const CONTENT_DIR = resolve(process.cwd(), "content");
 const BASE_PATH = process.env.NEXT_BASE_PATH || ""
 const ASSET_PREFIX = process.env.NEXT_ASSET_PREFIX || BASE_PATH
-const PAGES = glob.sync(`${CONTENT_DIR}/*.mdx`)
+const CONTENT_DIR = resolve(process.cwd(), "content");
+const PAGE_PATHS = glob.sync(`${CONTENT_DIR}/*.mdx`)
   .map(pagePath => pagePath
     .replace(CONTENT_DIR, '') // Make relative
     .replace(/\.mdx$/, '') // Remove extension
@@ -20,7 +20,7 @@ const PAGES = glob.sync(`${CONTENT_DIR}/*.mdx`)
 
 module.exports = configureNext({
   env: {
-    pagePaths: PAGES
+    pagePaths: PAGE_PATHS
   },
   basePath: BASE_PATH,
   assetPrefix: ASSET_PREFIX
